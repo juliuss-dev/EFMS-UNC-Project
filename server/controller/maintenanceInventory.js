4a9fa844f9ed0f6aede865504d573e70d0210f6d
Mambamel-dev <57382354+Mambamel-dev@users.noreply.github.com>
2022-04-06 22:03:21 +0800

    Update

--- server/controller/maintenanceInventory.js
+++ server/controller/maintenanceInventory.js
@@ -1,75 +1,80 @@
-const MaintenanceInventory = require('../model/MaintenanceInventory')
+const MaintenanceInventory = require("../model/MaintenanceInventory");
 
-exports.create = async (req, res) =>{
+exports.create = async (req, res) => {
+  const { name, quantity, description, dateAdded, status } = req.body;
 
-    const {name, quantity, description, dateAdded, status} = req.body
+  try {
+    let maintenanceInventory = new MaintenanceInventory();
 
-    try {
-        let maintenanceInventory = new MaintenanceInventory()   
-        
-        maintenanceInventory.name = name
-        maintenanceInventory.quantity = quantity
-        maintenanceInventory.description = description
-        maintenanceInventory.dateAdded = dateAdded
-        maintenanceInventory.status = status
+    maintenanceInventory.name = name;
+    maintenanceInventory.quantity = quantity;
+    maintenanceInventory.description = description;
+    maintenanceInventory.dateAdded = dateAdded;
+    maintenanceInventory.status = status;
 
-        await maintenanceInventory.save()
+    await maintenanceInventory.save();
 
-        res.json({
-            // reservation: reservation,
-            successMessage: "Equipment has successfully added"
-        })
-    } catch (error) {
-        console.log(error, "MaintenanceInventory POST Controller Error")
-        res.status(500).json({
-            errorMessage: "Try again, Maintenance Inventory Error"
-        })
-    }
-}
-
-exports.readAll = async (req,res) =>{
-    try {
-        const maintenanceInventory = await MaintenanceInventory.find({})
-        //return the maintenanceInventory
-        res.json({maintenanceInventory})
-        console.log(maintenanceInventory)
-    } catch (error) {
-        console.log("MaintenanceInventory GET ALL Controller Error")
-        res.status(500).json({
-            errorMessage: "Try Again, Maintenance Inventroy Error"
-        })
-    }
-}
-
-exports.update = async (req, res) =>{
-    try {
-        const inventoryId = req.params.inventoryId
-
-        const updateinventory = await MaintenanceInventory.findByIdAndUpdate(inventoryId, req.body)
-        console.log("Equipment successfully updated")
-        res.json({
-            successMessage: "Equipment successfully updated"
-        })
-    } catch (error) {
-        console.log('Update equipment error', error)
-        res.status(500).json({
-            errorMessage: "Update Equipment Error"
-        })
-    }
-}
-
-exports.delete = async(req, res) =>{
-   try {
-    const inventoryId = req.params.inventoryId
-    const deleteUpdate = await MaintenanceInventory.findByIdAndDelete(inventoryId, res.body)
-    console.log("Equipment successfully delete")
     res.json({
-        successMessage: "An equipment has been deleted"
-    })
-   } catch (error) {
-    console.log("Delete Equipment error",error)
+      // reservation: reservation,
+      successMessage: "Equipment has successfully added",
+    });
+  } catch (error) {
+    console.log(error, "MaintenanceInventory POST Controller Error");
     res.status(500).json({
-        errorMessage: "Delete Equipment error"
-    })
-   }
-}
+      errorMessage: "Try again, Maintenance Inventory Error",
+    });
+  }
+};
+
+exports.readAll = async (req, res) => {
+  try {
+    const maintenanceInventory = await MaintenanceInventory.find({});
+    //return the maintenanceInventory
+    res.json({ maintenanceInventory });
+    console.log(maintenanceInventory);
+  } catch (error) {
+    console.log("MaintenanceInventory GET ALL Controller Error");
+    res.status(500).json({
+      errorMessage: "Try Again, Maintenance Inventroy Error",
+    });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const inventoryId = req.params.inventoryId;
+
+    const updateinventory = await MaintenanceInventory.findByIdAndUpdate(
+      inventoryId,
+      req.body
+    );
+    console.log("Equipment successfully updated");
+    res.json({
+      successMessage: "Equipment successfully updated",
+    });
+  } catch (error) {
+    console.log("Update equipment error", error);
+    res.status(500).json({
+      errorMessage: "Update Equipment Error",
+    });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const inventoryId = req.params.inventoryId;
+    const deleteUpdate = await MaintenanceInventory.findByIdAndDelete(
+      inventoryId,
+      res.body
+    );
+    console.log("Equipment successfully delete");
+    res.json({
+      successMessage: "An equipment has been deleted",
+    });
+  } catch (error) {
+    console.log("Delete Equipment error", error);
+    res.status(500).json({
+      errorMessage: "Delete Equipment error",
+    });
+  }
+};
